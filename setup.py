1d861e4603840374768055fe3cd5bf689d9998fb
Teddy Crepineau <teddy.crepineau@gmail.com>
2018-11-10 15:02:41 -0800

    update config.yml syntax mistakes

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools.command.install import install
 import os
 import sys
 
-VERSION = '0.0.2git a'
+VERSION = '0.0.3'
 
 with open("README.md", "r") as f:
     long_description = f.read()
